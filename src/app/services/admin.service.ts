d8e9b9b4b630638e02c38e7091a94497b416343d
Akash Singa <akashsinga@gmail.com>
2020-08-09 10:35:02 +0530

    final project

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ParkingLot } from '../models/Location';
 import { GetReports } from '../Dto/GetReports';
+import { StorageService } from './storage-service.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,50 +11,61 @@ import { GetReports } from '../Dto/GetReports';
 export class AdminService {
 
   private url = 'http://localhost:8080/admin';
-  constructor(private httpclient:HttpClient) {}
+  constructor(private httpclient:HttpClient,private storage:StorageService) {}
+
+  getHeaders()
+  {
+    const options={
+      headers:new HttpHeaders({
+        'Content-Type':'application/json',
+        'request_id':String(this.storage.getUser()['id'])
+      })
+    };
+    return options;
+  }
 
   getDashboardCounts():Observable<any>
   {
-    return this.httpclient.get(this.url+"/dashboard/getCounts");
+    return this.httpclient.get(this.url+"/dashboard/getCounts",this.getHeaders());
   }
 
   getParkingLocations():Observable<any>
   {
-    return this.httpclient.get(this.url+"/locations/get");
+    return this.httpclient.get(this.url+"/locations/get",this.getHeaders());
   }
 
   addParkingLocation(location:ParkingLot):Observable<any>
   {
-    return this.httpclient.post(this.url+"/parkings/add",location);
+    return this.httpclient.post(this.url+"/parkings/add",location,this.getHeaders());
   }
   
   editParkingLocation(location:ParkingLot,id:number):Observable<any>
   {
-    return this.httpclient.post(this.url+"/parkings/edit/"+id,location);
+    return this.httpclient.post(this.url+"/parkings/edit/"+id,location,this.getHeaders());
   }
 
   deleteParkingLot(id:number):Observable<any>
   {
-    return this.httpclient.get(this.url+"/parkings/delete/"+id);
+    return this.httpclient.get(this.url+"/parkings/delete/"+id,this.getHeaders());
   }
 
   getUsers():Observable<any>
   {
-    return this.httpclient.get(this.url+"/users");
+    return this.httpclient.get(this.url+"/users",this.getHeaders());
   }
   
   editUser(id:number,role:string):Observable<any>
   {
-    return this.httpclient.post(this.url+"/users/edit/"+id,role);
+    return this.httpclient.post(this.url+"/users/edit/"+id,role,this.getHeaders());
   }
 
   getReports(reports:GetReports):Observable<any>
   {
-    return this.httpclient.post(this.url+"/reports",reports);
+    return this.httpclient.post(this.url+"/reports",reports,this.getHeaders());
   }
 
   cancelBooking(id:any):Observable<any>
   {
-    return this.httpclient.get(this.url+"/bookings/cancel/"+id);
+    return this.httpclient.get(this.url+"/bookings/cancel/"+id,this.getHeaders());
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ReserveParking } from '../Dto/ReserveParking';
 import { CheckAvailability } from '../Dto/CheckAvailability';
@@ -9,32 +9,43 @@ import { StorageService } from './storage-service.service';
 })
 export class UserService {
 
-  private url="http://localhost:8080/user/";
+  private url="http://localhost:8080/user";
   constructor(private httpclient:HttpClient,private storage:StorageService) { }
+  
+  getHeaders()
+  {
+    const options={
+      headers:new HttpHeaders({
+        'Content-Type':'application/json',
+        'request_id':String(this.storage.getUser()['id'])
+      })
+    };
+    return options;
+  }
 
   getLocations():Observable<any>
   {
-    return this.httpclient.get(this.url+"dashboard/getLocations");
+    return this.httpclient.get(this.url+"/dashboard/getLocations",this.getHeaders());
   }
   
   reserveParking(reservation:ReserveParking):Observable<any>
   {
-    return this.httpclient.post(this.url+"reserve",reservation);
+    return this.httpclient.post(this.url+"/reserve",reservation,this.getHeaders());
   }
 
   getPreviousBookings():Observable<any>
   {
     let user=this.storage.getUser();
-    return this.httpclient.get(this.url+"bookings/"+user['id']);  
+    return this.httpclient.get(this.url+"/bookings/"+user['id'],this.getHeaders());  
   }
 
   isAvailable(check:CheckAvailability):Observable<any>
   {
-    return this.httpclient.post(this.url+"parkings/lots/available",check);
+    return this.httpclient.post(this.url+"/parkings/lots/available",check,this.getHeaders());
   }
 
   cancelBooking(id:number):Observable<any>
   {
-    return this.httpclient.get(this.url+"bookings/cancel/"+id);
+    return this.httpclient.get(this.url+"/bookings/cancel/"+id,this.getHeaders());
   }
 }
